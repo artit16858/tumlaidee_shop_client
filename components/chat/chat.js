b91a5d1a8fca805c62bafffadfd3943aeafa41cc
Maxky2208 <maxky2208@gmail.com>
2020-09-26 11:41:54 +0700

    dataSource

---
+++ components/chat/chat.js
@@ -0,0 +1,14 @@
+
+const ChatView = (props) => {
+    const { data } = props;
+    return (
+        <div>
+            {data.map((item, idx) => (
+                <div className={`` + (item.user === 'max' ? 'text-right' : 'text-left')} key={idx} >
+                    <label className="text-message">{item.message}</label>
+                </div>
+            ))}
+        </div>
+    );
+}
+export default ChatView;
--- pages/chat.js
+++ pages/chat.js
@@ -1,373 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from "next/link";
 import PropTypes from 'prop-types';
 import Router, { withRouter } from 'next/router'
 import Head from 'next/head';
 import { CaretLeftOutlined, SendOutlined } from '@ant-design/icons';
-
-import ListChat from '../components/chat/list.js'
-
-
+import ChatView from '../components/chat/chat.js'
+import * as dataSource from '../data/chat/data.json';
 const Chat = (props) => {
     const { router } = props;
-    console.log("router.query", router.query);
-    console.log("router.query", router.query.user);
-    let data = [
-        {
-            id: 'max',
-            contents: [
-                {
-                    name: 'add',
-                    picture: '/assets/images/1.jpg',
-                    endedAt: 1570454329003,
-                    messages: [
-                        {
-                            user: 'add',
-                            message: '9999',
-                            isRead: true
-                        },
-                        {
-                            user: 'add',
-                            message: '식사중입니다.',
-                            isRead: true
-                        },
-                        {
-                            user: 'add',
-                            message: '언제쯤 오세요?',
-                            isRead: false
-                        },
-                        {
-                            user: 'max',
-                            message: '얼른 오세요~99999',
-                            isRead: false
-                        },
-                        {
-                            user: 'add',
-                            message: '얼른 오세요~',
-                            isRead: false
-                        },
-                        {
-                            user: 'max',
-                            message: '얼른 오세요~8915',
-                            isRead: false
-                        }
-                    ]
-                },
-                {
-                    name: '신정근 바텐더',
-                    picture: '/assets/images/3.jpg',
-                    endedAt: 1570551864061,
-                    messages: [
-                        {
-                            user: '신정근 바텐더',
-                            message: '회장님',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '네?',
-                            isRead: true
-                        },
-                        {
-                            user: '신정근 바텐더',
-                            message: '오시는 길에 와인좀 사다주세요!',
-                            isRead: false
-                        }
-                    ]
-                },
-                {
-                    name: '이미라 의사',
-                    picture: '/assets/images/4.jpg',
-                    endedAt: 1570887765818,
-                    messages: [
-                        {
-                            user: '최준원 회장님',
-                            message: '미라님',
-                            isRead: true
-                        },
-                        {
-                            user: '이미라 의사',
-                            message: '네?',
-                            isRead: true
-                        },
-                        {
-                            user: '이미라 의사',
-                            message: '말씀하세요!',
-                            isRead: false
-                        }
-                    ]
-                },
-                {
-                    name: '구찬성 지배인',
-                    picture: '/assets/images/6.jpg',
-                    endedAt: 1570531864061,
-                    messages: [
-                        {
-                            user: '최준원 회장님',
-                            message: '찬성님',
-                            isRead: true
-                        },
-                        {
-                            user: '구찬성 지배인',
-                            message: '네?',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '휴가 잘보내고 계세요?',
-                            isRead: true
-                        }
-                    ]
-                },
-                {
-                    name: '노준석 총지배인',
-                    picture: '/assets/images/5.jpg',
-                    endedAt: 1570532864061,
-                    messages: [
-                        {
-                            user: '최준원 회장님',
-                            message: '노준석 총지배인님',
-                            isRead: true
-                        },
-                        {
-                            user: '노준석 총지배인',
-                            message: '네?',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '건강은 좀 어떠세요?',
-                            isRead: true
-                        }
-                    ]
-                },
-                {
-                    name: '김유나 인턴',
-                    picture: '/assets/images/7.jpg',
-                    endedAt: 1570232864061,
-                    messages: [
-                        {
-                            user: '김유나 인턴',
-                            message: '안녕하세요!',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '새로오신 분인가요?',
-                            isRead: true
-                        },
-                        {
-                            user: '김유나 인턴',
-                            message: '네 잘부탁드립니다.',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '환영합니다 :D',
-                            isRead: true
-                        },
-                    ]
-                }
-            ],
-        },
-        {
-            id: 'add',
-            contents: [
-                {
-                    name: 'max',
-                    picture: '/assets/images/8.jpg',
-                    endedAt: new Date("2020/09/22").getTime(),
-                    messages: [
-                        {
-                            user: '장만월 사장님',
-                            message: '회장님 어디세요?',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '식사중입니다.',
-                            isRead: true
-                        },
-                        {
-                            user: '장만월 사장님',
-                            message: '언제쯤 오세요?',
-                            isRead: true
-                        },
-                        {
-                            user: '장만월 사장님',
-                            message: '얼른 오세요~',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: 'asdasd',
-                            isRead: true
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            id: '신정근 바텐더',
-            contents: [
-                {
-                    name: '최준원 회장님',
-                    picture: '/assets/images/8.jpg',
-                    endedAt: 1570551864061,
-                    messages: [
-                        {
-                            user: '신정근 바텐더',
-                            message: '회장님',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '네?',
-                            isRead: true
-                        },
-                        {
-                            user: '신정근 바텐더',
-                            message: '오시는 길에 와인좀 사다주세요!',
-                            isRead: true
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            id: '이미라 의사',
-            contents: [
-                {
-                    name: '최준원 회장님',
-                    picture: '/assets/images/8.jpg',
-                    endedAt: 1570887765818,
-                    messages: [
-                        {
-                            user: '최준원 회장님',
-                            message: '미라님',
-                            isRead: true
-                        },
-                        {
-                            user: '이미라 의사',
-                            message: '네?',
-                            isRead: true
-                        },
-                        {
-                            user: '이미라 의사',
-                            message: '말씀하세요!',
-                            isRead: true
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            id: '구찬성 지배인',
-            contents: [
-                {
-                    name: '최준원 회장님',
-                    picture: '/assets/images/8.jpg',
-                    endedAt: 1570531864061,
-                    messages: [
-                        {
-                            user: '최준원 회장님',
-                            message: '찬성님',
-                            isRead: true
-                        },
-                        {
-                            user: '구찬성 지배인',
-                            message: '네?',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '휴가 잘보내고 계세요?',
-                            isRead: true
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            id: '노준석 총지배인',
-            contents: [
-                {
-                    name: '최준원 회장님',
-                    picture: '/assets/images/8.jpg',
-                    endedAt: 1570532864061,
-                    messages: [
-                        {
-                            user: '최준원 회장님',
-                            message: '노준석 총지배인님',
-                            isRead: true
-                        },
-                        {
-                            user: '노준석 총지배인',
-                            message: '네?',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '건강은 좀 어떠세요?',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '건강은 좀 어떠세요?',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '건강은 좀 어떠세요?',
-                            isRead: true
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            id: '김유나 인턴',
-            contents: [
-                {
-                    name: '최준원 회장님',
-                    picture: '/assets/images/8.jpg',
-                    endedAt: 1570232864061,
-                    messages: [
-                        {
-                            user: '김유나 인턴',
-                            message: '안녕하세요!',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '새로오신 분인가요?',
-                            isRead: true
-                        },
-                        {
-                            user: '김유나 인턴',
-                            message: '네 잘부탁드립니다.',
-                            isRead: true
-                        },
-                        {
-                            user: '최준원 회장님',
-                            message: '환영합니다 :D',
-                            isRead: true
-                        },
-                    ]
-                }
-            ]
-        }
-    ];
-    const new_data = data.find(val => val.id === "max")
-    const messages = new_data.contents.find(val => val.name === router.query.user)
-    console.log("messages", messages);
-
+    const data = dataSource.data
     const [state, setState] = useState({
         user: router.query.user,
-        messages: messages ? messages.messages : []
+        messages: []
     });
-    console.log("state.query", state);
-
+    useEffect(() => {
+        readMessages();
+    }, []);
+    const readMessages = () => {
+        const new_data = data.find(val => val.id === "max")
+        const messages = new_data.contents.find(val => val.name === state.user)
+        setState({
+            ...state,
+            messages: messages ? messages.messages : []
+        })
+    };
     return (
         <div>
             <Head>
@@ -376,7 +32,6 @@ const Chat = (props) => {
                 <meta name="description" content="React Socket.io Chatting application" />
                 <meta name="keywords" content="react,socket.io,chatting,javascript" />
             </Head>
-
             <div className="navbar navbar-white sticky-top bg-head-chat flex-md-nowrap p-0" >
                 <div className="navbar-brand col-sm-3 col-md-2 mr-0" >
                     <Link href={`/list`}>
@@ -388,25 +43,16 @@ const Chat = (props) => {
                 </div>
             </div>
             <div className="container-chat">
-                {state.messages.map((item, idx) => (
-                    <div className={`` + (item.user === 'max' ? 'text-right' : 'text-left')} key={idx} >
-                        <label className="text-message">{item.message}</label>
-                    </div>
-                )
-                )}
-                {/* <main>{state.data && <ListChat user={state.user} data={state.data} />}</main> */}
+                <main>{state.messages && <ChatView data={state.messages} />}</main>
             </div>
             <div className="d-flex  justify-content-center" >
                 <input className="input-chat" type="text" />
                 <SendOutlined className="send-message" />
             </div>
-
         </div>
     )
 }
-
 export default withRouter(Chat)
-
 Chat.propTypes = {
     router: PropTypes.object,
 };
